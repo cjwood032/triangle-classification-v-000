a084a4098d1d4983b4509e6647e26cf6a6d08765
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-06-13 13:07:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   def kind
   if @a+@b<=@c||@a+@c<=@b||@c+@b<=@a
     TriangleError
-  elsif @a<0||@b<0||@c<0
+  elsif @a<=0||@b<=0||@c<=0
     TriangleError
   else
     if @a==@b&&@b==@c
